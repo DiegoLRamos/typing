995f85b13922c77adadd6e7a56a654c4171da41c
DiegoLRamos <diegolimaramos@gmail.com>
2021-10-09 16:07:18 -0300

    add project

--- src/App.js
+++ src/App.js
@@ -1,18 +1,93 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+
+import wordList from './resources/words.json';
+
+const MAX_TYPED_KEYS = 30;
+
+const getWord = () => {
+  const index = Math.floor(Math.random() * wordList.length);
+  const word = wordList[index];
+
+  return word.toLocaleLowerCase();
+}
+
+const isValidKey = (key, word) => {
+  if(!word) return false;
+
+  const result = word.split('').includes(key);
+  return result;
+};
+
+const Word = ({word, validKeys}) => {
+  if(!word) return null;
+  
+  const joinedKeys = validKeys.join('');
+  const matched = word.slice(0, joinedKeys.length);
+  const remainder = word.slice(joinedKeys.length);
+
+  return(
+    <>
+      <span className="matched">{matched}</span>
+      <span className="remainder">{remainder}</span>
+    </>
+  );
+};
 
 const App = () => {
+  const [typedKeys, setTypedKeys] = useState([]);
+  const [validKeys, setValidKeys] = useState([]);
+  const [completedWords, setCompletedWords] = useState([]);
+  const [word, setWord] = useState('');
+
+  useEffect(() => {
+    setWord(getWord());
+  }, []);
+
+  useEffect(() => {
+    const wordFromValidKeys = validKeys.join('').toLocaleLowerCase();
+    if(word && word === wordFromValidKeys){
+      let newWord = null;
+
+      do{
+        newWord = getWord();
+      }while(completedWords.includes(newWord));
+
+      setWord(newWord);
+      setValidKeys([]);
+      setCompletedWords((prev) => [...prev, word]);
+    }
+  }, [word, validKeys, completedWords]);
+
+  const handleKeyDown = (e) => {
+    e.preventDefault();
+    const { key } = e;
+    setTypedKeys((prevTypedKeys) => [...prevTypedKeys, key].slice(MAX_TYPED_KEYS * -1));
+
+    if(isValidKey(key, word)){
+      setValidKeys((prevTypedKeys) => {
+        const isValidLength = prevTypedKeys.length <= word.length;
+        const isNextChar = isValidLength && word[prevTypedKeys.length] === key;
+
+        return isNextChar ? [...prevTypedKeys, key] : prevTypedKeys;
+      });
+    }
+
+  };
+
   return(
-    <div className="container">
+    <div className="container" tabIndex="0" onKeyDown={handleKeyDown}>
       <div className="valid-keys">
-        <span className="matched">Die</span>
-        <span className="remainder">go</span>
+        <Word word={word} validKeys={validKeys}/>
       </div>
-      <div className="typed-keys">asdfsadf</div>
+      <div className="typed-keys">{typedKeys ? typedKeys.join(' ') : null}</div>
       <div className="completed-words">
         <ol>
-          <li>asdjfhaksljdhf</li>
-          <li>sdfgsdfgsdfg</li>
-          <li>sdfgsdf</li>
+          {completedWords.map((word) => {
+            return (
+              <li key={word}>{word}</li>
+            );
+          })}
         </ol>
       </div>
     </div>
